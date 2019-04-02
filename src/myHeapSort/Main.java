94c9f06ffdd32f2ed5887ad5c95f81ae9722f42b
bogdanpasterak <bogdanpasterak@gmail.com>
2019-04-02 22:41:43 +0100

    Print Array

--- src/myHeapSort/Main.java
+++ src/myHeapSort/Main.java
@@ -1,10 +1,15 @@
 package myHeapSort;
 
+import java.util.Arrays;
+
 public class Main {
 
 	public static void main(String[] args) {
+		int[] arr = {15, 17, 73, 21, 45, 11, 87, 55, 26, 17, 91, 17, 12, 55, 99};
+		
 		System.out.println("Start");
-
+		System.out.println(Arrays.toString(arr));
+		
 	}
 
 }
