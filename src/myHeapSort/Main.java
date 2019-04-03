ee1d4415290d28ee336337c4c5991f492ac89745
bogdanpasterak <bogdanpasterak@gmail.com>
2019-04-03 09:15:30 +0100

    Print Heap

--- src/myHeapSort/Main.java
+++ src/myHeapSort/Main.java
@@ -10,6 +10,39 @@ public class Main {
 		System.out.println("Start");
 		System.out.println(Arrays.toString(arr));
 		
+		printHeap(arr);
+	}
+
+	private static void printHeap(int[] arr) {
+		// number of rows in heap 
+		int rows = (int)Math.floor(Math.log(arr.length)/Math.log(2)) + 1;
+		String offset;
+		int count = 0;
+		
+		// loop for print row
+		for (int row = 0; row < rows; row++) {
+			
+			// offset on front
+			offset = "";
+			for (int i = 0; i < Math.pow(2, rows - row) - 2; i++)
+				offset += " ";
+			System.out.print(offset);
+			
+			// offset between
+			offset = "";
+			for (int i = 0; i < Math.pow(2, rows - row + 1) - 2; i++)
+				offset += " ";
+			
+			// print numbers
+			for (int col = 0; col < Math.pow(2, row); col++) {
+				if (count < arr.length) {
+					System.out.print(arr[count++] + offset);
+				}
+			}
+			
+			System.out.println();
+		}
+		System.out.println("------------------------------");
 	}
 
 }
